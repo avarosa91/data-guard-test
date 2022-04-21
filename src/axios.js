b36c6dd3db098094e1091130fa14661d4fc197ee
Julia Ivanova <jeivano91@gmail.com>
2022-04-21 09:16:22 +0300

    DEV-2 added server script

--- src/axios.js
+++ src/axios.js
@@ -8,7 +8,7 @@ Vue.use(VueAxios, axios);
 axios.defaults.headers.common.Accept = 'application/json';
 
 // Base URL
-axios.defaults.baseURL = process.env.URL || 'http://localhost:3000';
+axios.defaults.baseURL = `https://data-guard-test.herokuapp.com:${process.env.PORT}`;
 
 export default {
   axios,
